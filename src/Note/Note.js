a0028385b96dc2b9605e90f3cb815c5252e92f86
Henry Ebomah <henryebomah@gmail.com>
2021-02-07 09:04:13 -0800

    Proptypes

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import {format} from 'date-fns'
 import ApiContext from '../ApiContext'
@@ -66,4 +67,14 @@ export default class Note extends React.Component {
       </div>
     )
   }
+}
+
+Note.defaultProps = {
+  id: "",
+  name: ""
+}
+
+Note.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
 }
